bd6b4289fef6fa8040a47ce827a247d312ef84b9
niassagd <emailgchang@gmail.com>
2025-01-08 01:17:52 -0500

    Create 16 by 16 grid of squares

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const container = document.getElementById("container");
+
 const div = document.createElement("div");
+container.appendChild(div);
 
+const square = document.createElement("div");
 
-for (let i = 0; i < 16*16; i++) { 
+for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
-    div.classList.add("piece");
+    for (let i = 0; i < 16; i++) { 
+        const square = document.createElement("div");
+        square.classList.add("square");
+        div.appendChild(square);
+    }
     container.appendChild(div);
 }
-
