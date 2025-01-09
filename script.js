c69e80ca5cc77dc6bea65db3bd20c267b8c1f6bf
niassagd <emailgchang@gmail.com>
2025-01-08 22:54:13 -0500

    Add button to prompt user for number of rows/columns to create new grid

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 const container = document.getElementById("container");
 
-const row = document.createElement("div");
-row.classList.add("row");
-container.appendChild(row);
 
-const square = document.createElement("div");
+let n = 16;
 
-for (let i = 0; i < 25; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let i = 0; i < 25; i++) { 
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+//function to create grid
+function drawGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let i = 0; i < n; i++) { 
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
 
-        square.addEventListener("mouseenter",() => {
-            square.style.backgroundColor = "black";
-        });
+            square.addEventListener("mouseenter",() => {
+                square.style.backgroundColor = "black";
+            });
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
-
-
 }
+
+drawGrid(n);
+
+//function to delete grid
+
+let m = container.children.length;
+
+function deleteGrid() {
+    let m = container.children.length;
+    for(let i = 0; i < m; i++) {
+        const oldRow = container.firstElementChild;
+        container.removeChild(oldRow);
+    }
+}
+
+
+//button for prompt to ask to change rows/columns to
+const btn = document.querySelector("button");
+function promptFunction() {
+    let n = prompt("Enter number between 1 and 100):", "25");
+        if (n > 100) {
+            deleteGrid();
+            drawGrid(100);
+            alert("A max grid of 100 x 100 has been created");
+        } else {
+            deleteGrid();
+            drawGrid(n);
+        }
+    
+}
+btn.addEventListener("click", promptFunction);
+
+
+
+
 
     
 
